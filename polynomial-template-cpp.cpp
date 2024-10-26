c51300a0239a646195ac512cf6e5a097aed4a0dc
Klimenko George <george_klimenko6@mail.ru>
2024-10-26 16:59:40 +0300

    Initial commit

---
+++ polynomial-template-cpp.cpp
@@ -0,0 +1,149 @@
+﻿#include "polynomial-template-cpp.h"
+
+#include <vector>
+
+template <typename T>
+Polynomial<T>::Polynomial(const std::vector<T>& k) {
+
+}
+
+template <typename T>
+Polynomial<T>::Polynomial(const T& kf) {
+
+}
+
+template <typename T>
+template <typename Iter>
+Polynomial<T>::Polynomial(Iter begin, Iter end) {
+
+}
+
+template <typename T>
+bool Polynomial<T>::operator == (const Polynomial<T>& rhs) const {
+	return false;
+}
+
+template <typename T>
+bool Polynomial<T>::operator == (const T& rhs) const  {
+	return false;
+}
+
+template <typename T>
+bool Polynomial<T>::operator != (const Polynomial<T>& rhs) const {
+	return false;
+}
+
+template <typename T>
+bool Polynomial<T>::operator != (const T& rhs) const {
+	return false;
+}
+
+template <typename T>
+Polynomial<T>& Polynomial<T>::operator += (const Polynomial<T>& rhs) {
+
+}
+
+template <typename T>
+Polynomial<T>& Polynomial<T>::operator += (const T& rhs) {
+
+}
+
+template <typename T>
+Polynomial<T>& Polynomial<T>::operator -= (const Polynomial<T>& rhs) {
+
+}
+
+template <typename T>
+Polynomial<T>& Polynomial<T>::operator -= (const T& rhs) {
+
+}
+
+template <typename T>
+Polynomial<T>& Polynomial<T>::operator *= (const Polynomial<T>& rhs) {
+
+}
+
+template <typename T>
+Polynomial<T>& Polynomial<T>::operator *= (const T& rhs) {
+
+}
+
+template <typename T>
+Polynomial<T> Polynomial<T>::operator + (const Polynomial<T>& rhs) const {
+
+}
+
+template <typename T>
+Polynomial<T> Polynomial<T>::operator + (const T& rhs) const {
+
+}
+
+template <typename T>
+Polynomial<T> Polynomial<T>::operator - (const Polynomial<T>& rhs) const {
+
+}
+
+template <typename T>
+Polynomial<T> Polynomial<T>::operator - (const T& rhs) const {
+
+}
+
+template <typename T>
+Polynomial<T> Polynomial<T>::operator * (const Polynomial<T>& rhs) const {
+
+}
+
+template <typename T>
+Polynomial<T> Polynomial<T>::operator * (const T& rhs) const {
+
+}
+
+template <typename T>
+const T& Polynomial<T>::operator [] (size_t i) const {
+
+}
+
+template <typename T>
+int Polynomial<T>::Degree() const {
+
+}
+
+template <typename T>
+T Polynomial<T>::operator () (const T& point) const {
+
+}
+
+/*
+	Дальше методы переопределяются, чтобы все работало нормально
+*/
+
+template <typename T>
+bool operator == (const T& lhs, const Polynomial<T>& rhs) {
+
+}
+
+template <typename T> 
+bool operator != (const T& lhs, const Polynomial<T>& rhs) {
+
+}
+
+template <typename T>
+Polynomial<T> operator + (const T& lhs, const Polynomial<T>& rhs) {
+
+}
+
+template <typename T>
+Polynomial<T> operator - (const T& lhs, const Polynomial<T>& rhs) {
+
+}
+
+template <typename T>
+Polynomial<T> operator * (const T& lhs, const Polynomial<T>& rhs) {
+
+}
+
+// Вывод от старшей степени к младшей (reversed)
+template <typename T>
+std::ostream& operator << (std::ostream& out, const Polynomial<T>& val) {
+
+}
---
+++ polynomial-template-cpp.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+
+#include <vector>
+#include <iostream>
+
+template <typename T>
+class Polynomial {
+private:
+	std::vector<T> data;
+
+public:
+	Polynomial(const std::vector<T>& k);
+	Polynomial(const T& kf);
+
+	// по итераторам
+	template <typename Iter>
+	Polynomial(Iter begin, Iter end);
+
+	bool operator == (const Polynomial<T>& rhs) const;
+	bool operator == (const T& rhs) const;
+
+	bool operator != (const Polynomial<T>& rhs) const;
+	bool operator != (const T& rhs) const;
+
+	Polynomial<T>& operator += (const Polynomial<T>& rhs);
+	Polynomial<T>& operator += (const T& rhs);
+
+	Polynomial<T>& operator -= (const Polynomial<T>& rhs);
+	Polynomial<T>& operator -= (const T& rhs);
+
+	Polynomial<T>& operator *= (const Polynomial<T>& rhs);
+	Polynomial<T>& operator *= (const T& rhs);
+
+	Polynomial<T> operator + (const Polynomial<T>& rhs) const;
+	Polynomial<T> operator + (const T& rhs) const;
+
+	Polynomial<T> operator - (const Polynomial<T>& rhs) const;
+	Polynomial<T> operator - (const T& rhs) const;
+
+	Polynomial<T> operator * (const Polynomial<T>& rhs) const;
+	Polynomial<T> operator * (const T& rhs) const;
+
+	const T& operator [] (size_t i) const;
+
+	// Степень многочлена
+	int Degree() const;
+
+	// Вычисление многочлена в точке
+	T operator () (const T& point) const;
+
+	// Константные итераторы
+	using const_iterator = decltype(data.cbegin());
+	// Начиная с первого НЕ нуля
+	const_iterator begin() const {
+
+	}
+	const_iterator end() const {
+
+	}
+};
+
+
+template <typename T>
+bool operator == (const T& lhs, const Polynomial<T>& rhs);
+
+template <typename T>
+bool operator != (const T& lhs, const Polynomial<T>& rhs);
+
+template <typename T>
+Polynomial<T> operator + (const T& lhs, const Polynomial<T>& rhs);
+
+template <typename T>
+Polynomial<T> operator - (const T& lhs, const Polynomial<T>& rhs);
+
+template <typename T>
+Polynomial<T> operator * (const T& lhs, const Polynomial<T>& rhs);
+
+// Вывод от старшей степени к младшей (reversed)
+template <typename T>
+std::ostream& operator << (std::ostream& out, const Polynomial<T>& val);
